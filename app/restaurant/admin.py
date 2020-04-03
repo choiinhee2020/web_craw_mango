e00cc7306f5f571e1ebd16833b779fbd5d82e3ad
kimdooh2019 <choiinhee2020@gmail.com>
2020-04-03 21:55:36 +0900

    location category restaurant images data test

--- app/restaurant/admin.py
+++ app/restaurant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from restaurant.models import Restaurant
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Restaurant, RestaurantAdmin)
